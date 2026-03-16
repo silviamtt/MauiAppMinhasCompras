12b6745918fe2bd332a2baf1d6c0995d051ac2bb
Silviamtt <silviamtt@gmail.com>
2026-03-15 22:20:22 -0300

    Modificações da Agenda 04

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,22 +1,78 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
 
-private void ToolbarItem_Clicked(object sender, EventArgs e)
-    {
+	protected async override void OnAppearing()
+	{
+		try
+		{
+			List<Produto> tmp = await App.Db.GetAll();
+
+			tmp.ForEach(i => lista.Add(i));
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlertAsync("Ops", ex.Message, "OK");
+		}
+	}
+
+	private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
 		try
 		{
 			Navigation.PushAsync(new Views.NovoProduto());
 
-		} catch (Exception ex)
+		}
+		catch (Exception ex)
 		{
 			DisplayAlertAsync("Ops", ex.Message, "OK");
 		}
 
-    }
+	}
+
+	private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		try
+		{
+			string q = e.NewTextValue;
+
+			lista.Clear();
+
+			List<Produto> tmp = await App.Db.Search(q);
+
+			tmp.ForEach(i => lista.Add(i));
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlertAsync("Ops", ex.Message, "OK");
+		}
+	}
+
+	private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+	{
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O valor total dos produtos é: {soma:C}";
+
+		DisplayAlertAsync("Total dos Produtos", msg, "OK");
+	}
+
+	private async void SwipeItem_Clicked(object sender, EventArgs e)
+	{
+	
+
+	}
+
 }
